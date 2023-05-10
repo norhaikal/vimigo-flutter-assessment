e9cea21ecb0fed6a45ee9b648552c61b4883477d
norhaikal <123435122+norhaikal@users.noreply.github.com>
2023-05-10 11:24:12 +0800

    second-commit

---
+++ vimigo_flutter_asessment/android/app/src/main/kotlin/com/example/vimigo_flutter_asessment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vimigo_flutter_asessment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ vimigo_flutter_asessment/lib/data/dataset.dart
@@ -0,0 +1,44 @@
+import 'package:vimigo_flutter_asessment/data/attendance.dart';
+
+List<Attendance> dataset = [
+  Attendance(
+      user: "Chan Saw Lin",
+      phone: "0152131113",
+      checkIn: DateTime.parse('2020-06-30 16:10:05')),
+  Attendance(
+      user: "Lee Saw Loy",
+      phone: "0161231346",
+      checkIn: DateTime.parse('2020-07-11 15:39:59')),
+  Attendance(
+      user: "Khaw Tong Lin",
+      phone: "0158398109",
+      checkIn: DateTime.parse('2020-08-19 11:10:18')),
+  Attendance(
+      user: "Lim Kok Lin",
+      phone: "0168279101",
+      checkIn: DateTime.parse('2020-08-19 11:11:35')),
+  Attendance(
+      user: "Low Jun Wei",
+      phone: "0112731912",
+      checkIn: DateTime.parse('2020-08-15 13:00:05')),
+  Attendance(
+      user: "Yong Weng Kai",
+      phone: "0172332743",
+      checkIn: DateTime.parse('2020-06-30 16:10:05')),
+  Attendance(
+      user: "Jayden Lee",
+      phone: "0191236439",
+      checkIn: DateTime.parse('2020-06-30 16:10:05')),
+  Attendance(
+      user: "Kong Kah Yan",
+      phone: "0111931233",
+      checkIn: DateTime.parse('2020-06-30 16:10:05')),
+  Attendance(
+      user: "Jasmine Lau",
+      phone: "0162879190",
+      checkIn: DateTime.parse('2020-06-30 16:10:05')),
+  Attendance(
+      user: "Chan Saw Lin",
+      phone: "016783239",
+      checkIn: DateTime.parse('2020-06-30 16:10:05')),
+];
